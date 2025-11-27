bd2ea4edd77d7c85364256c858487f2c4ebbee58
Denis Badin <denisbadin@yandex.ru>
2025-11-27 18:57:06 +0500

    одна модель

--- services/models.py
+++ services/models.py
@@ -2,7 +2,12 @@ from django.db import models
 
 class RecipientMailing (models.Model):
     """Модель получатель рассылки"""
-    pass
+    email = models.EmailField(unique=True, verbose_name='Адрес электронный почты')
+    first_name = models.CharField(max_length=150, verbose_name='Имя')
+    last_name = models.CharField(max_length=150, verbose_name='Фамилия')
+    middle_name = models.CharField(max_length=150, verbose_name='Отчество')
+    comment = models.TextField()
+
 
 class Message (models.Model):
     """Модель сообщения"""
