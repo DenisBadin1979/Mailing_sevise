e30f9b869536922d05cddc422c6e6921e6f55ce2
Denis_Badin <denisbadin@yandex.ru>
2025-11-28 01:12:34 +0500

    Созданы три модели и админка, определены поля.

--- services/admin.py
+++ services/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import RecipientMailing, Message, Mailing
 
-# Register your models here.
+@admin.register(RecipientMailing)
+class RecipientMailingAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'middle_name', 'email')
+    list_filter = ('last_name',)
+    search_fields = ('last_name', 'first_name', 'middle_name', 'email')
+
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('subject_message', 'body_message')
+    search_fields = ('subject_message', 'body_message')
+
+
+@admin.register(Mailing)
+class MailingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'message', 'start_datetime', 'end_datetime', 'status')
+    list_filter = ('status', 'start_datetime')
+    filter_horizontal = ('recipients',)
+    readonly_fields = ('created_at', 'updated_at')
--- services/models.py
+++ services/models.py
@@ -3,19 +3,79 @@ from django.db import models
 class RecipientMailing (models.Model):
     """Модель получатель рассылки"""
     email = models.EmailField(unique=True, verbose_name='Адрес электронный почты')
-    first_name = models.CharField(max_length=150, verbose_name='Имя')
     last_name = models.CharField(max_length=150, verbose_name='Фамилия')
-    middle_name = models.CharField(max_length=150, verbose_name='Отчество')
-    comment = models.TextField()
+    first_name = models.CharField(max_length=150, verbose_name='Имя')
+    middle_name = models.CharField(max_length=150, blank=True, verbose_name='Отчество')
+    comment = models.TextField(blank=True, verbose_name='Комментарий')
+
+    class Meta:
+        verbose_name = ('Получатель рассылки')
+        verbose_name_plural = ('Получатели рассылки')
+        ordering = ['last_name', 'first_name']
+
+    def __str__(self):
+        return f'{self.last_name} {self.first_name} {self.middle_name}'
+
+
 
 
 class Message (models.Model):
     """Модель сообщения"""
-    pass
+    subject_message = models.CharField(max_length=100, verbose_name='Тема письма')
+    body_message = models.TextField(verbose_name='Тело сообщения')
 
-class Mailing (models.Model):
-    """Модель рассылка"""
-    pass
+    class Meta:
+        verbose_name = ('Сообщение')
+        verbose_name_plural = ('Сообщения')
+        ordering = ['subject_message']
+
+    def __str__(self):
+        return f'{self.subject_message}'
+
+class Mailing(models.Model):
+    """Модель рассылки"""
+    class Status(models.TextChoices):
+        COMPLETED = 'completed', ('Завершена')
+        CREATED = 'created', ('Создана')
+        STARTED = 'started', ('Запущена')
+
+    start_datetime = models.DateTimeField(verbose_name='Дата и время первой отправки')
+    end_datetime = models.DateTimeField(verbose_name='Дата и время окончания отправки')
+    status = models.CharField(
+        max_length=20,
+        choices=Status.choices,
+        default=Status.CREATED,
+        verbose_name='Статус'
+    )
+    message = models.ForeignKey(
+        Message,
+        on_delete=models.CASCADE,
+        related_name='mailings',
+        verbose_name='Сообщение'
+    )
+    recipients = models.ManyToManyField(
+        RecipientMailing,
+        related_name='mailings',
+        verbose_name='Получатели'
+    )
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата обновления')
+
+    class Meta:
+        verbose_name = 'Рассылка'
+        verbose_name_plural = 'Рассылки'
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f"Рассылка #{self.id} - {self.message.subject_message}"
+
+    @property
+    def is_active(self):
+        """Проверяет, активна ли рассылка в данный момент"""
+        from django.utils import timezone
+        now = timezone.now()
+        return (self.start_datetime <= now <= self.end_datetime and
+                self.status == self.Status.STARTED)
 
 class AttemptMailing (models.Model):
     """Модель попытки рассылки"""
